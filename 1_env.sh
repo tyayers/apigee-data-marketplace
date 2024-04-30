5aa2f904d62467f42f6414380941524f3ea52382
tyayers <tyler.ayers@gmail.com>
2024-04-30 10:56:06 +0200

    updated codelab

--- 1_env.sh
+++ 1_env.sh
@@ -2,8 +2,12 @@
 export NAME=
 export PROJECT_ID=
 export REGION=
-export CLOUD_RUN_URL=
-# The admin email will be given admin access to the marketplace, and also receive approval requests
-export ADMIN_EMAIL=
+
 # Internal domains are the email domains that will be considered internal for users registering (no approval needed)
 export INTERNAL_DOMAINS=
+
+# The admin email will be given admin access to the marketplace, and also receive approval requests
+export ADMIN_EMAIL=
+
+# This needs to be set before deploying to the Cloud Run with the URL
+export CLOUD_RUN_URL=
